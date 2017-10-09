af83b67b72574d4ff96d06501d138d969717bce2
chatzimano <stchatzim@gmail.com>
2017-10-09 16:06:13 +0100

    tidy

--- src/main/java/services/GameService.java
+++ src/main/java/services/GameService.java
@@ -1,5 +1,6 @@
 package main.java.services;
 
+import org.apache.log4j.Logger;
 import org.hibernate.Session;
 
 import main.java.DataAccessObject;
@@ -8,29 +9,35 @@ import main.java.dtos.Result;
 
 /**
  * manages adding game results
+ * 
  * @author stylian
  *
  */
 public class GameService {
 
+	final static Logger logger = Logger.getLogger(GameService.class);
+
 	private Session session;
-	
+
 	public GameService(Session session) {
 		this.session = session;
 	}
-	
+
 	public void addResult(Game game, Result result) {
-		
+
 		addScoreToGame(game, result);
-		
-		//TODO add stats to teams
-		
+
+		// TODO add stats to teams
+
 	}
 
 	private void addScoreToGame(Game game, Result result) {
+		logger.info("adding game result to game " + game.getHomeTeam() + " - " + game.getAwayTeam() + "  "
+				+ result.getGoalsMadeByHomeTeam() + " - " + result.getGoalsMadeByAwayTeam());
+
 		DataAccessObject<Game> gameDao = new DataAccessObject<>(session);
 		game.setResult(result);
 		gameDao.save(game);
 	}
-	
+
 }
