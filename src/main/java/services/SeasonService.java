22fb95e421c03a2d7dd5e375575126136d7eabf8
chatzimano <stchatzim@gmail.com>
2017-10-23 14:50:43 +0100

    tidy

--- src/main/java/services/SeasonService.java
+++ src/main/java/services/SeasonService.java
@@ -9,6 +9,7 @@ import org.hibernate.Session;
 import main.java.DataAccessObject;
 import main.java.PropertyUtils;
 import main.java.dtos.Group;
+import main.java.dtos.Season;
 import main.java.dtos.Team;
 
 public class SeasonService {
@@ -60,33 +61,33 @@ public class SeasonService {
 		
 	}
 	
-	public void createQualsRound() {
-		
-		//load team TODO
-		
-		Properties properties = PropertyUtils.load();
-		String strSeasonNum = properties.getProperty("season");
-		
-		int year = Integer.parseInt(strSeasonNum) + 1;
-		properties.setProperty("season", Integer.toString(year));
-		PropertyUtils.save(properties);
-		
-		logger.info("creating season " + year);
-		
-		Group group = new Group();
-		group.setName("season-" + year);
-		
-		DataAccessObject<Team> dao = new DataAccessObject<>(session);
-		List<Team> teams = dao.list("TEAMS");
-		
-		TeamsService teamService = new TeamsService(session);
-		
-		for(Team team : teams) {
-			
-			teamService.addTeamToGroup(group, team);
-			
-		}
-		
-	}
+//	public void createQualsRound() {
+//		
+//		//load team TODO
+//		
+//		Properties properties = PropertyUtils.load();
+//		String strSeasonNum = properties.getProperty("season");
+//		
+//		int year = Integer.parseInt(strSeasonNum) + 1;
+//		properties.setProperty("season", Integer.toString(year));
+//		PropertyUtils.save(properties);
+//		
+//		logger.info("creating season " + year);
+//		
+//		Group group = new Group();
+//		group.setName("season-" + year);
+//		
+//		DataAccessObject<Team> dao = new DataAccessObject<>(session);
+//		List<Team> teams = dao.list("TEAMS");
+//		
+//		TeamsService teamService = new TeamsService(session);
+//		
+//		for(Team team : teams) {
+//			
+//			teamService.addTeamToGroup(group, team);
+//			
+//		}
+//		
+//	}
 	
 }
