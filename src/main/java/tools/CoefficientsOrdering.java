f5ef4f7bfaa11ae0e78cab68588a8852dfb13df3
Stylian <stchatzim@gmail.com>
2017-11-01 15:18:01 +0000

    Update CoefficientsOrdering.java

--- src/main/java/tools/CoefficientsOrdering.java
+++ src/main/java/tools/CoefficientsOrdering.java
@@ -3,6 +3,8 @@ package main.java.tools;
 import main.java.dtos.Team;
 import main.java.dtos.groups.Group;
 
+// todo, this is not coeffs ordering, coeffs will not need to set the master group.
+// todo group ordering as well
 public class CoefficientsOrdering extends Ordering {
 	
 	public CoefficientsOrdering(Group group) {
