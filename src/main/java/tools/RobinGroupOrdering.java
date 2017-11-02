52525dff770391aab38c69a7f32b03ce404a690b
chatzimano <stchatzim@gmail.com>
2017-11-02 15:36:16 +0000

    added robin groups ordering rules

--- src/main/java/tools/RobinGroupOrdering.java
+++ src/main/java/tools/RobinGroupOrdering.java
@@ -1,8 +1,12 @@
 package main.java.tools;
 
+import org.apache.commons.lang3.RandomUtils;
+
+import main.java.HibernateUtils;
 import main.java.dtos.Stats;
 import main.java.dtos.Team;
 import main.java.dtos.groups.Group;
+import main.java.services.ServiceUtils;
 
 public class RobinGroupOrdering extends Ordering {
 	
@@ -21,7 +25,7 @@ public class RobinGroupOrdering extends Ordering {
 			return s2.getPoints() - s1.getPoints();
 		}
 		
-		// games between teams may not be worth
+		// RULE 1.5 *games between teams may not be worth
 		
 		// RULE 2
 		if(s1.getGoalDifference() != s2.getGoalDifference()) {
@@ -29,8 +33,22 @@ public class RobinGroupOrdering extends Ordering {
 		}
 		
 		// RULE 3
+		if(s1.getGoalsScored() != s2.getGoalsScored()) {
+			return s2.getGoalsScored() - s1.getGoalsScored();
+		}
+
+		// RULE 4
+		if(s1.getWins() != s2.getWins()) {
+			return s2.getWins() - s1.getWins();
+		}
 		
+		// RULE 5
+		Group master = ServiceUtils.getMasterGroup(HibernateUtils.getSessionFactory().getCurrentSession());
+		if(o1.getStatsForGroup(master).getPoints() != o2.getStatsForGroup(master).getPoints()) {
+			return o2.getStatsForGroup(master).getPoints() - o1.getStatsForGroup(master).getPoints();
+		}
 		
-		return o2.getStatsForGroup(group).getPoints() - o1.getStatsForGroup(group).getPoints();
+		// RULE 6
+		return RandomUtils.nextInt(-1, 1);
 	}
 }
