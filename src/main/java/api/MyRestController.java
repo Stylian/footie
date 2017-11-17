e54248888aa5b1114806e163a5c855afba6bf1e2
chatzimano <stchatzim@gmail.com>
2017-11-17 13:48:22 +0000

    separated rest with pages

--- src/main/java/api/MyRestController.java
+++ src/main/java/api/MyRestController.java
@@ -1,18 +1,19 @@
 package api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class MyController {
-
+@RestController
+public class MyRestController {
+	
   @Autowired
   private MyService myService;
 	
-  @RequestMapping("data/league")
+  @RequestMapping("rest/league")
   public String redirToList(){
-      return "data/league";
+      return "hello";
   }
+  
   
 }
