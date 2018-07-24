a76af843397c33f7ba1aeb2ef63660192931b2e8
Stylian <stchatzim@gmail.com>
2018-07-24 15:59:17 +0300

    added tests for ordering tools

--- src/test/java/gr/manolis/stelios/footie/core/tools/CoefficientOrderingTest.java
+++ src/test/java/gr/manolis/stelios/footie/core/tools/CoefficientOrderingTest.java
@@ -12,36 +12,48 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import gr.manolis.stelios.footie.core.peristence.dtos.Stats;
 import gr.manolis.stelios.footie.core.peristence.dtos.Team;
 import gr.manolis.stelios.footie.core.peristence.dtos.groups.Group;
 
 @RunWith(MockitoJUnitRunner.class)
-public class AlphabeticalOrderingTest {
+public class CoefficientOrderingTest {
 
 	@Mock
 	Team Zagreb;
 
 	@Mock
 	Team Alexa;
-	
+
 	@Mock
 	Group group;
-	
+
+	@Mock
+	Stats statsZagreb;
+
+	@Mock
+	Stats statsAlexa;
+
 	@Test
 	public void testCompare() {
 
 		when(Zagreb.getName()).thenReturn("Zagreb");
 		when(Alexa.getName()).thenReturn("Alexa");
+		when(Zagreb.getStatsForGroup(group)).thenReturn(statsZagreb);
+		when(Alexa.getStatsForGroup(group)).thenReturn(statsAlexa);
 		
+		when(statsZagreb.getPoints()).thenReturn(5);
+		when(statsAlexa.getPoints()).thenReturn(7);
+
 		List<Team> list = new ArrayList<>();
 		list.add(Zagreb);
 		list.add(Alexa);
-		
-		Collections.sort(list, new AlphabeticalOrdering(group));
-		
+
+		Collections.sort(list, new CoefficientsOrdering(group));
+
 		assertEquals("Alexa", list.get(0).getName());
 		assertEquals("Zagreb", list.get(1).getName());
-		
+
 	}
-	
+
 }
