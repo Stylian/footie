57b0316614384b74dc5b92313a11251a508527c3
Stylian <stylianos.chatzimanolis@dotmatics.com>
2019-06-24 17:07:53 +0300

    done coeffs page

--- ui/src/components/history_components/Coefficients.jsx
+++ ui/src/components/history_components/Coefficients.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {AppBar, Tabs, Tab, Typography, TableHead, TableRow, TableCell, TableBody} from "@material-ui/core";
+import {TableHead, TableRow, TableCell, TableBody, Grid, Paper, Box} from "@material-ui/core";
 
 class Coefficients extends Component {
 
@@ -19,7 +19,7 @@ class Coefficients extends Component {
             .then(res => res.json())
             .then(
                 (result) => {
-                    this.setState( state => {
+                    this.setState(state => {
                         return {
                             ...state,
                             isLoaded: true,
@@ -28,7 +28,7 @@ class Coefficients extends Component {
                     });
                 },
                 (error) => {
-                    this.setState( state => {
+                    this.setState(state => {
                         return {
                             ...state,
                             isLoaded: true,
@@ -40,26 +40,70 @@ class Coefficients extends Component {
     }
 
     render() {
+
+        let firstTable = {};
+        let secondTable = {};
+        let length = Object.keys(this.state.teams).length;
+        Object.keys(this.state.teams).map((key, index) => {
+            if (index < length / 2) {
+                firstTable[key] = this.state.teams[key];
+            } else {
+                secondTable[key] = this.state.teams[key];
+            }
+        });
+
         return (
-            <table className = "table" >
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Pos</TableCell>
-                        <TableCell>Team</TableCell>
-                        <TableCell>Coefficients</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                {Object.keys(this.state.teams).map((key, index) => {
-                    return (
-                        <TableRow >
-                            <TableCell align="right" >{index+1}</TableCell >
-                            <TableCell >{key}</TableCell >
-                            <TableCell align="right" >{this.state.teams[key]}</TableCell >
-                        </TableRow >)
-                 })}
-                </TableBody>
-            </table>
+            <Box width={800} >
+            <Paper elevation={12}  style={{margin: 20}}>
+
+                <Grid container spacing={1}>
+                    <Grid item sm>
+                        <table className="table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Pos</TableCell>
+                                    <TableCell>Team</TableCell>
+                                    <TableCell>Coefficients</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {Object.keys(firstTable).map((key, index) => {
+                                    return (
+                                        <TableRow>
+                                            <TableCell align="right">{index + 1}</TableCell>
+                                            <TableCell>{key}</TableCell>
+                                            <TableCell align="right">{this.state.teams[key]}</TableCell>
+                                        </TableRow>)
+                                })}
+                            </TableBody>
+                        </table>
+                    </Grid>
+
+                    <Grid item sm>
+                        <table className="table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Pos</TableCell>
+                                    <TableCell>Team</TableCell>
+                                    <TableCell>Coefficients</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {Object.keys(secondTable).map((key, index) => {
+                                    return (
+                                        <TableRow>
+                                            <TableCell align="right">{length / 2 + index + 1}</TableCell>
+                                            <TableCell>{key}</TableCell>
+                                            <TableCell align="right">{this.state.teams[key]}</TableCell>
+                                        </TableRow>)
+                                })}
+                            </TableBody>
+                        </table>
+                    </Grid>
+                </Grid>
+            </Paper>
+                </Box>
+
         );
     }
 }
