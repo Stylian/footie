0eb6d19ecf814ae7d6ebfb5bbda7a9d32ec3d894
Stylian <stelium.x@gmail.com>
2024-03-03 00:14:59 +0200

    changing classes to functions

--- ui/src/components/Player.jsx
+++ ui/src/components/Player.jsx
@@ -1,108 +1,83 @@
-import React, {Component} from "react";
-import {Box, Card, CardContent, CardHeader, Grid, Paper, TableCell, TableRow} from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { Box, Card, CardContent, CardHeader, Grid, Paper, TableCell, TableRow } from "@material-ui/core";
 import LeagueToolbar from "./LeagueToolbar";
 import playeroftheyear from "../icons/playeroftheyear.png";
 import dreamteam from "../icons/dreamteam.png";
 import silvermedal from "../icons/silvermedal.png";
 import goldmedal from "../icons/goldmedal.png";
+import {useParams} from "react-router";
 
-class Player extends Component {
+export default function Player() {
+    const {playerId} = useParams();
 
-    constructor(props) {
-        super(props);
+    const [isLoaded, setLoaded] = useState(false);
+    const [player, setPlayer] = useState({});
 
-        this.state = {
-            isLoaded: false,
-            player: {},
-        };
-
-    }
-
-    componentDidMount() {
-        fetch("/rest/players/" + this.props.match.params.playerId)
+    useEffect(() => {
+        fetch("/rest/players/" + playerId)
             .then(res => res.json())
             .then(
                 (result) => {
-                    this.setState(state => {
-
-                        return {
-                            ...state,
-                            player: result,
-                            isLoaded: true,
-                        }
-                    });
+                    setPlayer(result);
+                    setLoaded(true);
                 },
                 (error) => {
-                    this.setState(state => {
-                        return {
-                            ...state,
-                            isLoaded: true,
-                            error
-                        }
-                    });
+                    console.error("Error fetching player:", error);
+                    setLoaded(true);
                 }
-            )
-    }
+            );
+    });
 
-    render() {
-        return (
-            this.state.isLoaded ? (
-                <Box>
-                    <Paper style={{margin: 20}} elevation={20}>
-                        <LeagueToolbar pageTitle={this.state.player.name}/>
+    return (
+        isLoaded ? (
+            <Box>
+                <Paper style={{ margin: 20 }} elevation={20}>
+                    <LeagueToolbar pageTitle={player.name} />
 
-                        <Box width={1700} style={{margin: 20}}>
-                            <Grid container spacing={1}>
-                                <Grid item sm={4}>
-                                    <Card style={{margin: 20}}>
-                                        <CardHeader title={"trophies"} align={"center"}
-                                                    titleTypographyProps={{variant: 'h7'}}
-                                        />
-                                        <CardContent>
-                                            {this.state.player.trophies.length > 0 ? (
-                                                <table className="table" align={"center"}>
-                                                    {this.state.player.trophies.map((trophy, index) => {
-                                                        return (
-                                                            <TableRow>
-                                                                <TableCell align="right">
-                                                                    {trophy.type == "W" ?
-                                                                        (<img src={goldmedal}
-                                                                              title={"championship"}/>) :
-                                                                        (trophy.type == "R" ?
-                                                                        (<img src={silvermedal}
-                                                                              title={"finalists"}/>) :
-                                                                        (trophy.type == "PL" ?
-                                                                        (<img src={playeroftheyear}
-                                                                              title={"player of the year"}/>) :
-                                                                        (<img src={dreamteam}
-                                                                              title={"dream team"}/>)))}
-                                                                </TableCell>
-                                                                <TableCell
-                                                                    align="right">{"Season " + trophy.seasonNum}</TableCell>
-                                                                <TableCell align="left">
-                                                                    {trophy.type == "W" ? "Championship" : (
-                                                                        (trophy.type == "R" ? "Finalists" :
-                                                                            (trophy.type == "PL" ?
-                                                                        "Player of the year" : "Dream Team")))}</TableCell>
-                                                            </TableRow>
-                                                        )
-                                                    })}
-                                                </table>
-                                            ) : (
-                                                <i>nothing in the trophies case</i>
-                                            )}
-                                        </CardContent>
-                                    </Card>
-                                </Grid>
+                    <Box width={1700} style={{ margin: 20 }}>
+                        <Grid container spacing={1}>
+                            <Grid item sm={4}>
+                                <Card style={{ margin: 20 }}>
+                                    <CardHeader title={"trophies"} align={"center"} titleTypographyProps={{ variant: 'h7' }} />
+                                    <CardContent>
+                                        {player.trophies.length > 0 ? (
+                                            <table className="table" align={"center"}>
+                                                {player.trophies.map((trophy, index) => (
+                                                    <TableRow key={index}>
+                                                        <TableCell align="right">
+                                                            {trophy.type === "W" ? (
+                                                                <img src={goldmedal} title={"championship"} alt="Gold Medal" />
+                                                            ) : trophy.type === "R" ? (
+                                                                <img src={silvermedal} title={"finalists"} alt="Silver Medal" />
+                                                            ) : trophy.type === "PL" ? (
+                                                                <img src={playeroftheyear} title={"player of the year"} alt="Player of the Year" />
+                                                            ) : (
+                                                                <img src={dreamteam} title={"dream team"} alt="Dream Team" />
+                                                            )}
+                                                        </TableCell>
+                                                        <TableCell align="right">{"Season " + trophy.seasonNum}</TableCell>
+                                                        <TableCell align="left">
+                                                            {trophy.type === "W" ? "Championship" : (
+                                                                trophy.type === "R" ? "Finalists" : (
+                                                                    trophy.type === "PL" ? "Player of the year" : "Dream Team"
+                                                                )
+                                                            )}
+                                                        </TableCell>
+                                                    </TableRow>
+                                                ))}
+                                            </table>
+                                        ) : (
+                                            <i>nothing in the trophies case</i>
+                                        )}
+                                    </CardContent>
+                                </Card>
                             </Grid>
-                        </Box>
-                    </Paper>
-                </Box>
-            ) : (
-                <span></span>
-            )
-        );
-    }
+                        </Grid>
+                    </Box>
+                </Paper>
+            </Box>
+        ) : (
+            <span></span>
+        )
+    );
 }
-
-export default Player;
