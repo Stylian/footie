4df0a395f33bd9fd421ab6cf602bb8233ffd7803
Stylian <stelium.x@gmail.com>
2024-03-04 13:11:36 +0200

    refactoring

--- ui/src/components/LeagueToolbar.jsx
+++ ui/src/components/LeagueToolbar.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -15,149 +15,124 @@ import players from "../icons/football-players.png";
 import analytics from "../icons/analytics.png";
 import barchart from "../icons/bar-chart.png";
 import medal1 from "../icons/medal1.png";
+import {useParams} from "react-router";
 
-class LeagueToolbar extends Component {
+function LeagueToolbar({ pageTitle, seasonNum }) {
 
-    constructor(props) {
-        super(props);
+    const [menuPosition, setMenuPosition] = useState(null);
+    const [seasonsTotal, setSeasonsTotal] = useState(0);
 
-        this.state = {
-            menuPosition: null,
-            seasonsTotal: 0,
-            currentDisplayedSeason: 1
-        };
-
-    }
-
-    componentDidMount() {
+    useEffect(() => {
         fetch("/rest/seasons/")
             .then(res => res.json())
             .then(
                 (result) => {
-                    this.setState(state => {
-
-                        return {
-                            ...state,
-                            isLoaded: true,
-                            seasonsTotal: result,
-                        }
-                    });
+                    setSeasonsTotal(result);
                 },
                 (error) => {
-                    this.setState(state => {
-                        return {
-                            ...state,
-                            isLoaded: true,
-                            error
-                        }
-                    });
+                    console.error('Error fetching seasons:', error);
                 }
-            )
+            );
+    }, []);
 
+    const handleClick = (event) => {
+        setMenuPosition(event.currentTarget);
     }
 
-    handleClick = (event) => {
-        this.setState({menuPosition: event.currentTarget});
-    }
-
-    handleButtonSelection = (e) => {
+    const handleButtonSelection = (e) => {
         window.location = e.currentTarget.dataset.link;
     }
 
-    handleClose = () => {
-        this.setState({menuPosition: null});
+    const handleClose = () => {
+        setMenuPosition(null);
     }
 
-    handleUp = (event) => {
-        window.location.href = parseInt(this.props.seasonNum) + 1;
+    const handleUp = () => {
+        window.location.href = parseInt(seasonNum) + 1;
     }
 
-    handleDown = (event) => {
-        window.location.href = parseInt(this.props.seasonNum) - 1;
+    const handleDown = () => {
+        window.location.href = parseInt(seasonNum) - 1;
     }
 
-    render() {
-        return (
-            <div>
-                <AppBar position="static">
-                    <Grid container spacing={10}>
-                        <Grid item xs={6}>
-                            <Toolbar>
-                                <IconButton edge="start" color="inherit" aria-label="Menu"
-                                            onClick={this.handleClick}>
-                                    <MenuIcon/>
-                                </IconButton>
-                                <Menu
-                                    id="simple-menu"
-                                    anchorEl={this.state.menuPosition}
-                                    anchorOrigin={{vertical: "bottom", horizontal: "left"}}
-                                    getContentAnchorEl={null}
-                                    keepMounted
-                                    open={Boolean(this.state.menuPosition)}
-                                    onClose={this.handleClose}
-                                >
-                                    <MenuItem data-link="/" onClick={this.handleButtonSelection}>
-                                        <ListItemIcon>
-                                            <img src={list} title={"Seasons"}/>
-                                        </ListItemIcon>
-                                        <ListItemText primary="Seasons"/>
-                                    </MenuItem>
-                                    <MenuItem data-link="/coefficients" onClick={this.handleButtonSelection}>
-                                        <ListItemIcon>
-                                            <img src={medal1} title={"Awards & Coefficients"}/>
-                                        </ListItemIcon>
-                                        <ListItemText primary="Awards & Coefficients"/>
-                                    </MenuItem>
-                                    <MenuItem data-link="/league_stats" onClick={this.handleButtonSelection}>
-                                        <ListItemIcon>
-                                            <img src={analytics} title={"League Stats"}/>
-                                        </ListItemIcon>
-                                        <ListItemText primary="League Stats"/>
-                                    </MenuItem>
-                                    <MenuItem data-link="/teams_stats" onClick={this.handleButtonSelection}>
-                                        <ListItemIcon>
-                                            <img src={barchart} title={"Teams' Stats"}/>
-                                        </ListItemIcon>
-                                        <ListItemText primary="Teams' Stats"/>
-                                    </MenuItem>
-                                    <MenuItem data-link="/players_edit" onClick={this.handleButtonSelection}>
-                                        <ListItemIcon>
-                                            <img src={players} title={"players"}/>
-                                        </ListItemIcon>
-                                        <ListItemText primary="Players"/>
-                                    </MenuItem>
-                                    <MenuItem data-link="/admin" onClick={this.handleButtonSelection}>
-                                        <ListItemIcon>
-                                            <img src={build} title={"admin"}/>
-                                        </ListItemIcon>
-                                        <ListItemText primary="Admin"/>
-                                    </MenuItem>
-                                </Menu>
+    return (
+        <div>
+            <AppBar position="static">
+                <Grid container spacing={10}>
+                    <Grid item xs={6}>
+                        <Toolbar>
+                            <IconButton edge="start" color="inherit" aria-label="Menu" onClick={handleClick}>
+                                <MenuIcon/>
+                            </IconButton>
+                            <Menu
+                                id="simple-menu"
+                                anchorEl={menuPosition}
+                                anchorOrigin={{vertical: "bottom", horizontal: "left"}}
+                                getContentAnchorEl={null}
+                                keepMounted
+                                open={Boolean(menuPosition)}
+                                onClose={handleClose}
+                            >
+                                <MenuItem data-link="/" onClick={handleButtonSelection}>
+                                    <ListItemIcon>
+                                        <img src={list} title={"Seasons"}/>
+                                    </ListItemIcon>
+                                    <ListItemText primary="Seasons"/>
+                                </MenuItem>
+                                <MenuItem data-link="/coefficients" onClick={handleButtonSelection}>
+                                    <ListItemIcon>
+                                        <img src={medal1} title={"Awards & Coefficients"}/>
+                                    </ListItemIcon>
+                                    <ListItemText primary="Awards & Coefficients"/>
+                                </MenuItem>
+                                <MenuItem data-link="/league_stats" onClick={handleButtonSelection}>
+                                    <ListItemIcon>
+                                        <img src={analytics} title={"League Stats"}/>
+                                    </ListItemIcon>
+                                    <ListItemText primary="League Stats"/>
+                                </MenuItem>
+                                <MenuItem data-link="/teams_stats" onClick={handleButtonSelection}>
+                                    <ListItemIcon>
+                                        <img src={barchart} title={"Teams' Stats"}/>
+                                    </ListItemIcon>
+                                    <ListItemText primary="Teams' Stats"/>
+                                </MenuItem>
+                                <MenuItem data-link="/players_edit" onClick={handleButtonSelection}>
+                                    <ListItemIcon>
+                                        <img src={players} title={"players"}/>
+                                    </ListItemIcon>
+                                    <ListItemText primary="Players"/>
+                                </MenuItem>
+                                <MenuItem data-link="/admin" onClick={handleButtonSelection}>
+                                    <ListItemIcon>
+                                        <img src={build} title={"admin"}/>
+                                    </ListItemIcon>
+                                    <ListItemText primary="Admin"/>
+                                </MenuItem>
+                            </Menu>
 
-                                {this.props.seasonNum !== undefined ? (
-                                    <Box>
-                                        {this.props.seasonNum != this.state.seasonsTotal ? (
-                                        <IconButton onClick={this.handleUp}>
-                                            <img src={up} title={"next season"}/>
+                            {seasonNum !== undefined && (
+                                <Box>
+                                    {seasonNum !== seasonsTotal && (
+                                        <IconButton onClick={handleUp}>
+                                            <img src={up} title={"next season"} />
                                         </IconButton>
-                                        ) : ('')}
+                                    )}
+                                    {seasonNum > 1 && (
+                                        <IconButton onClick={handleDown}>
+                                            <img src={down} title={"previous season"} />
+                                        </IconButton>
+                                    )}
+                                </Box>
+                            )}
 
-                                        {this.props.seasonNum > 1 ? (
-                                            <IconButton onClick={this.handleDown}>
-                                                <img src={down} title={"previous season"}/>
-                                            </IconButton>
-                                        ) : ('')}
-                                    </Box>
-                                ) : ''}
-
-                                <Typography variant="h6">{this.props.pageTitle}</Typography>
-                            </Toolbar>
-                        </Grid>
+                            <Typography variant="h6">{pageTitle}</Typography>
+                        </Toolbar>
                     </Grid>
-                </AppBar>
-            </div>
-        );
-    }
+                </Grid>
+            </AppBar>
+        </div>
+    );
 }
 
 export default LeagueToolbar;
